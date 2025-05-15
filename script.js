73842c49b79f806e687217c3cf30181d379761d5
Soham Kundu <sohamkundu29012002@gmail.com>
2025-05-16 03:41:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+	let firstName = document.querySelector('input[name="fname"]');
+	let lastName = document.querySelector('input[name="lname"]');
+	let str = firstName.value + " " + lastName.value
+	alert(str)
 }
